7cb68a821de43bad33a29549085acd53265e3a56
Naomi <naomidie1311@gmail.com>
2025-11-17 20:11:02 +0200

    ReactListGoods_TS-ND

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import 'bulma/css/bulma.css';
 import './App.scss';
+import { useState } from 'react';
 
 export const goodsFromServer = [
   'Dumplings',
@@ -15,36 +16,112 @@ export const goodsFromServer = [
   'Garlic',
 ];
 
-export const App: React.FC = () => {
+enum SortType {
+  None = 'none',
+  Alphabetical = 'alpha',
+  Length = 'length',
+}
+
+export const App = () => {
+  const [goods, setGoods] = useState(goodsFromServer);
+  const [activeSort, setActiveSort] = useState<SortType>(SortType.None);
+  const [isReversed, setIsReversed] = useState(false);
+
+  const sortAlphabetically = () => {
+    const sorted = [...goods].sort((a, b) => a.localeCompare(b));
+
+    setGoods(isReversed ? sorted.reverse() : sorted);
+    setActiveSort(SortType.Alphabetical);
+  };
+
+  const sortByLength = () => {
+    const sorted = [...goodsFromServer].sort((a, b) => {
+      if (a.length !== b.length) {
+        return a.length - b.length;
+      }
+
+      return a.localeCompare(b);
+    });
+
+    setGoods(isReversed ? sorted.reverse() : sorted);
+    setActiveSort(SortType.Length);
+  };
+
+  const reverseList = () => {
+    const reversed = [...goods].reverse();
+
+    setGoods(reversed);
+    setIsReversed(!isReversed);
+  };
+
+  const resetList = () => {
+    setGoods(goodsFromServer);
+    setActiveSort(SortType.None);
+    setIsReversed(false);
+  };
+
+  const getButtonClass = (type: string) => {
+    if (type === 'info') {
+      return `button is-${type} ${activeSort === SortType.Alphabetical ? '' : 'is-light'}`;
+    }
+
+    if (type === 'success') {
+      return `button is-${type} ${activeSort === SortType.Length ? '' : 'is-light'}`;
+    }
+
+    if (type === 'warning') {
+      return `button is-${type} ${isReversed ? '' : 'is-light'}`;
+    }
+
+    return `button is-${type}`;
+  };
+
+  const isOriginalOrder = activeSort === SortType.None && !isReversed;
+
   return (
     <div className="section content">
       <div className="buttons">
-        <button type="button" className="button is-info is-light">
+        <button
+          type="button"
+          className={getButtonClass('info')}
+          onClick={sortAlphabetically}
+        >
           Sort alphabetically
         </button>
 
-        <button type="button" className="button is-success is-light">
+        <button
+          type="button"
+          className={getButtonClass('success')}
+          onClick={sortByLength}
+        >
           Sort by length
         </button>
 
-        <button type="button" className="button is-warning is-light">
+        <button
+          type="button"
+          className={getButtonClass('warning')}
+          onClick={reverseList}
+        >
           Reverse
         </button>
 
-        <button type="button" className="button is-danger is-light">
-          Reset
-        </button>
+        {!isOriginalOrder && (
+          <button
+            type="button"
+            className={getButtonClass('danger')}
+            onClick={resetList}
+          >
+            Reset
+          </button>
+        )}
       </div>
 
       <ul>
-        <ul>
-          <li data-cy="Good">Dumplings</li>
-          <li data-cy="Good">Carrot</li>
-          <li data-cy="Good">Eggs</li>
-          <li data-cy="Good">Ice cream</li>
-          <li data-cy="Good">Apple</li>
-          <li data-cy="Good">...</li>
-        </ul>
+        {goods.map(item => (
+          <li key={item} data-cy="Good">
+            {item}
+          </li>
+        ))}
       </ul>
     </div>
   );
